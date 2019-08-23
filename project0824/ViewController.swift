d11502c337d69bb79c9d3eba0e6fa5d85d082f27
MoongGi <heartboy08@nate.com>
2019-08-24 00:22:22 +0900

    Initial Commit

---
+++ project0824/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project0824
+//
+//  Created by 프리텔레콤 on 24/08/2019.
+//  Copyright © 2019 프리텔레콤. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
